b343231535084612d785f112fa9f9025d97d40c3
Alexey Azorabekov <a.azorabekov@presentsimple.com>
2025-03-16 15:26:07 +0300

    Fixed cors errors

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,45 @@ async function bootstrap() {
   
   // Настройка CORS для разрешения запросов с фронтенда
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://moos-doors.ru', 'https://www.moos-doors.ru', 'http://moos-doors.ru', 'http://www.moos-doors.ru'],
+    origin: [
+      'http://localhost:3000',
+      'https://moos-doors.ru',
+      'https://www.moos-doors.ru',
+      'http://moos-doors.ru',
+      'http://www.moos-doors.ru',
+      'https://doors-shop-frontend-production.up.railway.app',
+      'http://doors-shop-frontend-production.up.railway.app',
+      'https://doors-shop-backend-production.up.railway.app',
+      'http://doors-shop-backend-production.up.railway.app',
+      'null',
+      '*'
+    ],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Accept', 'Authorization', 'Origin', 'X-Requested-With'],
-    exposedHeaders: ['Content-Disposition'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+    allowedHeaders: [
+      'Content-Type',
+      'Accept',
+      'Authorization',
+      'Origin',
+      'X-Requested-With',
+      'Access-Control-Allow-Origin',
+      'Access-Control-Allow-Headers',
+      'Access-Control-Allow-Methods',
+      'Access-Control-Allow-Credentials',
+      'X-HTTP-Method-Override',
+      'Accept-Language',
+      'Access-Control-Expose-Headers',
+      'Access-Control-Max-Age',
+      'Referer',
+      'Host',
+      'X-Real-IP',
+      'X-Forwarded-For',
+      'Connection',
+      'Pragma',
+      'Cache-Control'
+    ],
+    exposedHeaders: ['Content-Disposition', 'X-RateLimit-Reset'],
+    maxAge: 3600,
   });
   
   app.setGlobalPrefix('api');
