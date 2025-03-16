ecb52c96b253cb5ca01bdffc91c927734bb44472
Alexey Azorabekov <a.azorabekov@presentsimple.com>
2025-03-16 10:54:13 +0300

    Fixed problems with api

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://moos-doors.ru', 'https://www.moos-doors.ru'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
@@ -13,6 +13,8 @@ async function bootstrap() {
   
   app.setGlobalPrefix('api');
 
-  await app.listen(3001);
+  const port = process.env.PORT || 9090;
+  await app.listen(port);
+  console.log(`Application is running on port ${port}`);
 }
 bootstrap();
