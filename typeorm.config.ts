e7bef21387285ff1239afc1c71a7d401a5fc88be
Alexey Azorabekov <a.azorabekov@presentsimple.com>
2025-03-17 22:42:20 +0300

    Fix troubles

--- typeorm.config.ts
+++ typeorm.config.ts
@@ -6,10 +6,8 @@ import { User } from './src/users/entities/user.entity';
 import { Order } from './src/orders/entities/order.entity';
 import { CreateTables1710000000000 } from './src/migrations/1710000000000-CreateTables';
 import { CreateOrders1710367200000 } from './src/migrations/1710367200000-CreateOrders';
-import { CreateDoorsTable1710424800000 } from './src/migrations/1710424800000-CreateDoorsTable';
 import { AddImageUrlsArray1710424800001 } from './src/migrations/1710424800001-AddImageUrlsArray';
 import { AddDoorDetails1710424800002 } from './src/migrations/1710424800002-AddDoorDetails';
-import { CreateOrdersTable1710500000000 } from './src/migrations/1710500000000-CreateOrdersTable';
 import { UpdateUserPasswords1710587682123 } from './src/migrations/UpdateUserPasswords';
 import { AddNewDoorFields1710510000000 } from './src/migrations/1710510000000-AddNewDoorFields';
 
@@ -24,15 +22,14 @@ const dataSource = new DataSource({
   database: process.env.DB_NAME || 'doors_repair',
   entities: [Door, Category, User, Order],
   migrations: [
-    CreateTables1710000000000,
-    CreateOrders1710367200000,
-    CreateDoorsTable1710424800000,
-    AddImageUrlsArray1710424800001,
-    AddDoorDetails1710424800002,
-    CreateOrdersTable1710500000000,
-    UpdateUserPasswords1710587682123,
-    AddNewDoorFields1710510000000
+    CreateTables1710000000000,         // Создает базовые таблицы (categories, doors)
+    CreateOrders1710367200000,         // Создает таблицу order с enum
+    AddImageUrlsArray1710424800001,    // Обновляет структуру imageUrls
+    AddDoorDetails1710424800002,       // Добавляет детали дверей
+    UpdateUserPasswords1710587682123,  // Обновляет пароли пользователей
+    AddNewDoorFields1710510000000      // Добавляет новые поля для дверей
   ],
+  migrationsRun: true,
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
   synchronize: false,
   logging: process.env.NODE_ENV === 'development',
