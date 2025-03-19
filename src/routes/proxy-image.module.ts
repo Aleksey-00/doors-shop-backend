52e4222291817526291d16706e91c724ca0594fe
Alexey Azorabekov <a.azorabekov@presentsimple.com>
2025-03-19 23:03:03 +0300

    Fixed troubles

--- src/routes/proxy-image.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ProxyImageController } from './proxy-image';
-
-@Module({
-  controllers: [ProxyImageController],
-})
-export class ProxyImageModule {} 
